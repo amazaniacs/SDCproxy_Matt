d9645e48a01eb955219c8e9f3b141602c97b9a15
doodoodoom <madridmatt17@gmail.com>
2019-03-11 14:48:21 -0700

    Implement http-proxy-middleware

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
+require('newrelic');
 const express = require('express');
 const compression = require('compression');
 const cors = require('cors');
+const proxy = require('http-proxy-middleware');
 
 const app = express();
 
+app.use('/api/productreview', proxy({
+  target: 'http://localhost:3008',
+  changeOrigin: true
+}));
+
+// const options = {
+//   target: '/',
+//   router: {
+//     '/api/productreview': 'http://localhost:3008',
+//     '/api/review': 'http://localhost:3008'
+//   }
+// };
+
+// const proxyServer = proxy(options);
+
+
+// app.use(proxyServer);
 app.use(compression());
 app.use(cors());
+
 
 const port = 5000;
 
